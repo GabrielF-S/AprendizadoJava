8b3c3003e8e7366808c36009f15a5c3496c99c6a
Gabriel <gabrielboni2008@hotmail.com>
2022-05-30 15:45:09 -0300

    Override do metodo ToString

--- lambdas/Produto.java
+++ lambdas/Produto.java
@@ -3,13 +3,20 @@ package lambdas;
 public class Produto {
 	final String nome;
 	final double preco;
-	final double disconto;
+	final double desconto;
 	
 	
-	public Produto(String nome, double preco, double disconto) {
+	public Produto(String nome, double preco, double desconto) {
 		this.nome = nome;
 		this.preco = preco;
-		this.disconto = disconto;
+		this.desconto = desconto;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		double valorFinal = (preco*(1-desconto));
+		return String.format("Produto: %s tem o preço de: %.2f", this.nome, valorFinal);
 	}
 	
 	
