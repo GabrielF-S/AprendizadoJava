bd039ff0f11192e904d76ea54edd5d26d8b113d3
Gabriel <gabrielboni2008@hotmail.com>
2022-06-03 20:23:27 -0300

    Aprendendo sobre o metódo Map

---
+++ streams/Map.java
@@ -0,0 +1,28 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Map {
+	public static void main(String[] args) {
+		Consumer<String> print = System.out::print;
+		
+		
+		List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda\n");
+		
+		marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+		
+
+		
+		//System.out.println(maiuscula.andThen(primeiraLetra).andThen(grito).apply("a"));
+		System.out.println("\n\nUsando composição");
+		marcas.stream().map(Utilitarios.maiuscula).map(Utilitarios.primeiraLetra).map(Utilitarios::grito).forEach(print);
+		
+		
+		
+		
+		
+	}
+
+}
