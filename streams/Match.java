f85727071a251c7014d9d6f46d6adde00e7c7bd8
Gabriel <gabrielboni2008@hotmail.com>
2022-06-08 18:10:43 -0300

    Aprendendo conceitos do metodo Match, all, any e none

---
+++ streams/Match.java
@@ -0,0 +1,29 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Match {
+	//compativel
+public static void main(String[] args) {
+		
+		Aluno a1 = new Aluno("Ana", 7.1);
+		Aluno a2 = new Aluno("Liz", 6.1);
+		Aluno a3 = new Aluno("Caua", 8.1);
+		Aluno a4 = new Aluno("Gui", 10);
+		Aluno a5 = new Aluno("Antonieta", 5.1);
+		
+		
+		
+		List<Aluno> alunos = Arrays.asList(a1,a2,a3,a4,a5);
+		
+		Predicate<Aluno> aprovado = a -> a.nota >= 7.0;
+		Predicate<Aluno> reprovado = aprovado.negate();
+		
+		System.out.println(alunos.stream().allMatch(aprovado));
+		System.out.println(alunos.stream().anyMatch(aprovado));
+		System.err.println(alunos.stream().noneMatch(reprovado));
+	}
+
+}
