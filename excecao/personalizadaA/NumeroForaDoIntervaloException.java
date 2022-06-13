10bcacc927865346ed1d0a8f2224bdd102853ecb
Gabriel <gabrielboni2008@hotmail.com>
2022-06-13 18:31:02 -0300

    Criação de uma exceção não verificada

---
+++ excecao/personalizadaA/NumeroForaDoIntervaloException.java
@@ -0,0 +1,19 @@
+package excecao.personalizadaA;
+
+@SuppressWarnings("serial")
+public class NumeroForaDoIntervaloException extends RuntimeException{
+	
+	private String nomeDoAtributo;
+	
+	public NumeroForaDoIntervaloException(String nome) {
+		this.nomeDoAtributo = nome;
+	}
+	
+	@Override
+	public String getMessage() {
+		return String.format("O atributo '%s' está fora do intervalo", nomeDoAtributo);
+	}
+	
+	
+
+}
---
+++ excecao/personalizadaA/TesteValidacao.java
@@ -0,0 +1,19 @@
+package excecao.personalizadaA;
+
+import excecao.Aluno;
+
+public class TesteValidacao {
+	public static void main(String[] args) {
+
+		try {
+			Aluno aluno = new Aluno("Ana", -7);
+			Validar.aluno(aluno);
+		} catch (StringVaziaException  | NumeroForaDoIntervaloException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("Fim :) ");
+	}
+
+}
